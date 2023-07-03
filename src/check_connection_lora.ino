5551fa32a1db0f1b6fac836d31dacf4d4b2a0920
Ricardo <ricardo.rg462@gmail.com>
2023-07-02 22:03:57 -0400

    commenting check_connection_lora.ino

--- src/check_connection_lora.ino
+++ src/check_connection_lora.ino
@@ -1,15 +1,16 @@
-int flag_B1 = 0;
-int count_t1 = 0;
-int count_E1 = 0;
-int E_1 = 0;
+int max_tries = 10;                                 // Definicion de maximos intentos de revision de conexion
+int flag_B1 = 0;                                    // Definicion de flag de estado de conexion
+int count_t1 = 0;                                   // Definicion de contador t1
+int count_E1 = 0;                                   // Definicion de contador de intentos fallidos
+int E_1 = 0;                                        // Definicion de flag de error de conexion
 
 
-void check_connection_lora(){
+void check_connection_lora(){                       // Se definen los estados
     enum Connection_Lora{
-        COUNT,
-        CHECKING_CONNECTION,
-        CONNECTION_SUCCESSFUL,
-        CONNECTION_FAILED,
+        COUNT,                                      // Estado de contar
+        CHECKING_CONNECTION,                        // Estado de revisar conexion
+        CONNECTION_SUCCESSFUL,                      // Estado de conexion exitosa
+        CONNECTION_FAILED,                          // Estado de conexion fallida
     };
 
     Connection_Lora currentState;
@@ -21,47 +22,47 @@ void check_connection_lora(){
     {
     case CHECKING_CONNECTION:
 
-        if (flag_B1 == 1){
-            currentState = CONNECTION_SUCCESSFUL;
-            count_E1 = 0;
-            E_1 = 0;
+        if (flag_B1 == 1){                          // Si hay conexion exitosa
+            currentState = CONNECTION_SUCCESSFUL;   // Se cambia el estado a conexion exitosa
+            count_E1 = 0;                           // Se reinicia el contador de intentos fallidos
+            E_1 = 0;                                // Se lleva el flag de error a 0
         }
-        else{
-            currentState = COUNT;
+        else{                                       // En el caso contrario
+            currentState = COUNT;                   // Se va al estado de contar
         }
         break;
 
     //////////////////
     //  E2 - COUNT  //
     //////////////////
-    case COUNT:
-        count_t1 ++;
-        count_E1 ++;
-        if ((count_t1 > 1000) && (count_E1 = 10)){
-            currentState = CONNECTION_FAILED;
-            count_t1 = 0;
-            count_E1 = 0;
-            E_1 = 1;
+    case COUNT:                                     
+        count_t1 ++;                                // Se cuenta una iteracion en tiempo
+        count_E1 ++;                                // Se cuenta un intento fallido
+        if ((count_t1 > 1000) && (count_E1 = max_tries)){  // Si se supera el contador y se alcanza el numero maximo de intentos fallidos
+            currentState = CONNECTION_FAILED;       // Se va al estado de conexion fallida
+            count_t1 = 0;                           // Se reinicia el contador de tiempo
+            count_E1 = 0;                           // Se reinicia el contador de intentos fallidos
+            E_1 = 1;                                // Se lleva el flag de error a 1
         }
 
-        else if ((count_t1 > 1000) && (E_1 != 10)){
-            currentState = CHECKING_CONNECTION;
-            count_t1 = 0;
+        else if ((count_t1 > 1000) && (E_1 != max_tries)){ // Si se supera el contador y no se alcanza el numero maximo de intentos fallidos
+            currentState = CHECKING_CONNECTION;     // Se va al estado de revisar conexion
+            count_t1 = 0;                           // Se reinicia el contador de tiempo
         }
 
-        else{
-            currentState = COUNT;
+        else{                                       // En el caso contrario
+            currentState = COUNT;                   // Se va al estado de contar
         }
 
         break;
 
-    //////////////////////
-    //  E2 - SUCCESFUL  //
-    //////////////////////
+    ///////////////////////
+    //  E2 - SUCCESSFUL  //
+    ///////////////////////
     case CONNECTION_SUCCESSFUL:
-        count_t1 = 0;
-        count_E1 = 0;
-        E_1 = 0;
+        count_t1 = 0;                               // Se reinicia el contador de tiempo
+        count_E1 = 0;                               // Se reinicia el contador de intentos fallidos
+        E_1 = 0;                                    // Se lleva el flag de error a 0
         break;
     }
 }
